2f805f185f0affad8965b374e06ef9cba25218a8
Novgor <jegor.novosjolov28@gmail.com>
2026-03-03 00:34:15 +0200

    Com

---
+++ Assets/Resources/Scripts/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)  
+    {
+        if (collision.gameObject.CompareTag("Potion"))
+        {
+            Debug.Log("Potion consumed");
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Barrier"))
+        {
+            Debug.Log("Player collided with a barrier");
+        }
+        Debug.Log("collision");
+    }
+}
+
---
+++ Assets/Resources/Scripts/SpawnSection.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SpawnSection : MonoBehaviour
+{
+    [SerializeField] private GameObject DungeonSectionLit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered spawn trigger");
+            Instantiate(DungeonSectionLit, new Vector3 (transform.position.x+51,0,0), Quaternion.identity);
+        }
+    }
+}
+
